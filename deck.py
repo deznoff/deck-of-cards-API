9da17453fdb86ce73cb1556c7aaa16fc75a0e546
Savva Tretyakov <savvatretyakov@gmail.com>
2023-09-03 11:24:57 +0100

    Created CardGame class with methods
    Created a method with to get a deck id and return it to use in other
    methods.
    Created a method to return a body of drawn cards from a user defined
    number of cards.

--- deck.py
+++ deck.py
@@ -2,13 +2,23 @@ import requests
 import json
 
 class CardGame:
-    deck_response = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    deck_body = json.loads(deck_response.content)
-    deck_id = deck_body['deck_id']
+    def __init__(self):
+        deck_response = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        self.deck_body = json.loads(deck_response.content)
+    
+    #Create a method to get a Deck ID
+    def get_deck_id(self):
+        return self.deck_body['deck_id']
+
+    def return_drawn_card(self):
+        draw_count = input('Type in the number of cards to draw: ')
+        payload = {'count': draw_count}
+        return json.loads(requests.get(f'https://deckofcardsapi.com/api/deck/{self.get_deck_id()}/draw', 
+                                       params=payload).content)
+    
+
 
 card_game1 = CardGame()
+print(card_game1.return_drawn_card())
 
-
-draw_cnt = input('Type in a number of cards to draw: ')
-
-print(deck_id)
+#draw_cnt = input('Type in a number of cards to draw: ')
